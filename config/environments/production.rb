5b410ee0772d280a84d5d71ec39e17a6c2eee663
Tebayoso <tebayoso@gmail.com>
2014-08-25 10:06:32 -0300

    Removed asset problems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.assets.raise_asset_errors = false
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 

d02c3d6274e44bb679170a986a7b89aadfea5a94
Tebayoso <tebayoso@gmail.com>
2014-09-07 23:42:41 -0300

    Mandrill mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'www.redeperros.com' }
+  config.action_mailer.default_url_options = { host: 'redeperros.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address:    "smtp.mandrillapp.com",

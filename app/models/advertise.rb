1a8847de843a6fcecb179ec3d811fe0bdc9c565a
Tebayoso <tebayoso@gmail.com>
2014-09-11 19:23:37 -0300

    Modified images path to avoid adblock

--- app/models/advertise.rb
+++ app/models/advertise.rb
@@ -1,21 +1,21 @@
 class Advertise < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "750x450>", :thumb => "375x225>" },
   :default_style => :thumb,
-  :url => "/system/:class/:attachment/:id/:style/:basename.:extension",
-  :path => ":rails_root/public/system/:class/:attachment/:id/:style/:basename.:extension"
+  :url => "/system/:attachment/:id/:style/:basename.:extension",
+  :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
 
-  validates :comments, presence: true, length: { minimum: 50 }
+  validates :comments, presence: true, length: { minimum: 50, maximum: 80 }
   validates :status, presence: true
-  validates :color1, presence: true
-  validates :race, presence: true
-  validates :street1, presence: true
-  validates :street2, presence: true
-  validates :size, presence: true
+  validates :color1, presence: true, length: { maximum: 25 }
+  validates :race, presence: true, length: { maximum: 25 }
+  validates :street1, presence: true, length: { maximum: 25 }
+  validates :street2, presence: true, length: { maximum: 25 }
+  validates :size, presence: true, length: { maximum: 25 }
   validates :when, presence: true
-  validates :gender, presence: true
-  validates :age, presence: true
-  validates :city, presence: true
+  validates :gender, presence: true, length: { maximum: 25 }
+  validates :age, presence: true, length: { maximum: 2 }
+  validates :city, presence: true, length: { maximum: 25 }
 end
